53eb8282075aa6b208820e5c026719f2ffad9e2f
Carl Steib <carl.steib@gmail.com>
2020-08-25 22:29:20 -0700

    Add selector for colemak

--- src/dvorak/core.cljs
+++ src/dvorak/core.cljs
@@ -5,42 +5,58 @@
             [dvorak.translator :as d]))
 
 (def state
-  (r/atom {:input ""
-           :output ""
-           :tr ""}))
+  (r/atom {:input   ""
+           :output  ""
+           :tr      ""
+           :table   (d/make-table :dvorak :qwerty)
+           :r-table (d/reverse-table (d/make-table :dvorak :qwerty))}))
 
 (defn root-component
-  [input-ratom output-ratom tr-ratom dest-atom]
+  [table r-table input-ratom output-ratom tr-ratom dest-atom]
   [:div
+   [:h3 "option list"]
+   [:div.form-group
+    [:label "pick a source keyboard"]
+    [:select.form-control {:field     :list
+                           :id        :many.options
+                           :on-change (fn [new]
+                                        (let [to-kb     (.-value (.-target new))
+                                              new-table (d/make-table (keyword to-kb) :qwerty)]
+                                          (reset! table new-table)
+                                          (reset! r-table (d/reverse-table new-table))))}
+     [:option {:key :dvorak} "dvorak"]
+     [:option {:key :colemak} "colemak"]]]
    [:h3 "This is your input textbox."]
    [:textarea
-    {:value @input-ratom
+    {:value     @input-ratom
      :on-change (fn [new]
                   (let [new-txt (.-value (.-target new))]
                     (reset! input-ratom new-txt)
-                    (reset! output-ratom (d/convert-to-dvorak new-txt))))}
+                    (reset! output-ratom (d/convert (:table @state) new-txt))))}
     ]
    [:h3 "This is your QWERTY=>Dvorak simulated text"]
    [:textarea
-    {:value @output-ratom
+    {:value    @output-ratom
      :disabled true}]
    [:h3 "Try entering the text above into this textbox .."]
    [:textarea
-    {:value @tr-ratom
+    {:value     @tr-ratom
      :on-change (fn [evt]
                   (let [new-txt (.-value (.-target evt))]
                     (reset! tr-ratom new-txt)
-                    (reset! dest-atom (d/simulate-dvorak new-txt)))                  )
+                    (reset! dest-atom (d/convert (:r-table @state) new-txt))))
      }]
    [:h3 ".. which is as if you typed the original input in Dvorak"]
    [:textarea
-    {:value @dest-atom
+    {:value    @dest-atom
      :disabled true}
     ]
    ])
 
 (defn mountit []
   (rd/render [root-component
+              (r/cursor state [:table])
+              (r/cursor state [:r-table])
               (r/cursor state [:input])
               (r/cursor state [:output])
               (r/cursor state [:tr])
